89bcfb824b4d37676fa360f3dcb40c063760ff27
StefanoR <rubinetti.stefano@gmail.com>
2026-04-13 12:16:29 +0200

    v1

---
+++ src/esempi/eccezioni/Esempio2.java
@@ -0,0 +1,31 @@
+package esempi.eccezioni;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Esempio2
+{
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dammi nome file");
+        while(true) {
+            try {
+                String nome = sc.nextLine();
+                leggiFile(nome);
+                break;
+            } catch (FileNotFoundException e) {
+                System.out.println("dammi il nome di un file vero");
+            }
+        }
+    }
+
+    public static void leggiFile(String nomefile) throws FileNotFoundException
+    {
+        File ilMioFile = new File(nomefile);//non è un operazione pericolosa
+        //va passato come parametro allo Scanner
+        Scanner fr = new Scanner(ilMioFile);
+        while(fr.hasNextLine())
+            System.out.println(fr.nextLine());
+    }
+}
